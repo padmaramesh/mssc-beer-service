83531a64755c1f12ad944dff6773e122ab6b251f
padmaramesh <padma.ramesh@gmail.com>
2020-06-25 09:10:49 +0530

    Created the beer service spring boot project and updated with the data model.

---
+++ src/main/java/padma/ramesh/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package padma.ramesh.web.model;
+
+/**
+ * Created by ramesh.padma on 25-06-2020.
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON;
+}
