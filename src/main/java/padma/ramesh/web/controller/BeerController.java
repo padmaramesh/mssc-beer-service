f3db07c09a10d78dde31569c79cecf21d69ba875
padmaramesh <padma.ramesh@gmail.com>
2020-06-25 09:37:43 +0530

    updated with beer controller

---
+++ src/main/java/padma/ramesh/web/controller/BeerController.java
@@ -0,0 +1,35 @@
+package padma.ramesh.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import padma.ramesh.web.model.BeerDto;
+
+import java.util.UUID;
+
+/**
+ * Created by ramesh.padma on 25-06-2020.
+ */
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        //Todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto){
+
+        //TODO IMPL
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
